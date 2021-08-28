9977a32ae20ca461db942acf5263ebe2b7135b09
c-biancone <chris.biancone@gmail.com>
2021-08-28 13:29:41 -0400

    avg, min, max initial method

--- main.cpp
+++ main.cpp
@@ -13,14 +13,25 @@ int main() {
 	// Set the precision for cout
 	std::cout.precision(2);
 
+    // Prompt the user
+    std::cout << "Enter three integer numbers separated by a space or newline:" << std::endl;
+
 	// Get Three integers
 	std::cin >> x >> y >> z;
+    // Assume that only integers are provided
 
 	// Output Average (Show in fixed point)
 	std::cout << "Average: " << std::fixed << average(x, y, z) << std::endl;
 
-	// Finish
-	// Output Median, Min, Max
+    // Output Median (Show in fixed point)
+//    std::cout << "Median: " << std::fixed << median(x, y, z) << std::endl;
 
+    // Output Min (Show in fixed point)
+    std::cout << "Min: " << std::fixed << min(x, y, z) << std::endl;
+
+    //Output Max (Show in fixed point)
+    std::cout << "Max: " << std::fixed << max(x, y, z) << std::endl;
+
+	// Finish
 	return 0;
 }
--- stats.cpp
+++ stats.cpp
@@ -5,6 +5,60 @@
 
 double average(int x, int y, int z)
 {
+    int sum = x + y + z;
 
-  return 0;
+    // divide sum by number of inputs
+    return (sum / 3);
 }
+/*
+int median(int x, int y, int z)
+{
+    if (x < y) {
+        if (y<z) {
+            return y;
+        } else if (x<z) {
+            return z;
+        } else {
+            return x;
+        }
+    } else {
+        if (z>y) {
+            return y;
+        } else if (z>x) {
+            return x;
+        } else {
+            return z;
+        }
+    }
+}
+*/
+int min(int x, int y, int z)
+{
+    // temp variable for comparison
+    int tmp;
+
+    // compare
+    x<y ? tmp=x : tmp=y;
+    if (tmp>z)
+    {
+        tmp=z;
+    }
+
+    return tmp;
+}
+
+int max(int x, int y, int z)
+{
+    // temp variable for comparison
+    int tmp;
+
+    // compare
+    x>y ? tmp=x : tmp=y;
+    if (tmp<z)
+    {
+        tmp=z;
+    }
+
+    return tmp;
+}
+
